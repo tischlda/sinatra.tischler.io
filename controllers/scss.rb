e8d5b1dad97e7dfd37a5db2c6f3ca6b1f50b9b21
David Tischler <david@tischler.io>
2013-01-01 20:05:11 +1000

    last_modified for scss.

--- controllers/scss.rb
+++ controllers/scss.rb
@@ -11,7 +11,17 @@ class ScssController < BaseController
 
   get '/stylesheets/:name.css' do |name|
     begin
+      # Without this requests with If-Modified-Since for non existent
+      # files could falsely receive 304 instead of 404.
+      pass if not File.exists? "#{settings.views}/#{name}.scss"
+      
+      # Set Last-Modified to modification date of most recently modified
+      # .scss-file, as the one requested could include any other.
+      last_modified Dir.glob("#{settings.views}/**").map {|f| File.mtime(f) }.max
+
       scss name.to_sym
+
+    # raised by scss if file not found
     rescue Errno::ENOENT
       raise Sinatra::NotFound.new
     end
